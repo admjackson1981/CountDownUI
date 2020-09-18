fd7d68b6383f44e8655c80da5f592742697cd9a0
admjackson1981 <anthonyjackson_6@hotmail.com>
2020-09-18 10:52:03 +0100

    Initial commit

---
+++ Assets/Scripts/ClockManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ClockManager : MonoBehaviour
+{
+
+    private  TextMeshProUGUI _textmeshProgui;
+    public AudioSource audio1;
+    public AudioSource audio2;
+    private bool AudioAlternate = false;
+    public int wait = 1;
+    public AudioClip clockTick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        _textmeshProgui = FindObjectOfType<TextMeshProUGUI>();
+        StartCoroutine("ClockGO");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+       
+
+       
+    }
+    IEnumerator ClockGO()
+    {
+        while (true)
+        {
+             if(AudioAlternate)
+             {
+                 audio1.Play();
+                 AudioAlternate = false;
+                Debug.Log("play 1");
+             }
+             else
+             {
+                 AudioAlternate = true;
+                 audio2.Play();
+                Debug.Log("play 2");
+             }
+           
+          
+            timeCreation t = new timeCreation(DateTime.Now);
+
+             _textmeshProgui.SetText(t.ReturnTimingString.ToString());
+
+            yield return new WaitForSeconds(wait);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/timeCreation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeCreation 
+{
+   protected DateTime WeddingDay { get; private set; }
+    
+    public timeCreation(DateTime weddingDay)
+    {
+        WeddingDay = weddingDay;
+    }
+
+    public string ReturnTimingString {
+        get {
+
+            DateTime date1 =  DateTime.Now;
+            DateTime date2 = new DateTime(2022, 9, 20, 09, 30, 00);
+
+            // Calculate the interval between the two dates.
+            TimeSpan interval = date2 - date1;
+            
+            
+
+            string timeString = string.Format("{0}:{1}:{2}:{3}", interval.Days, interval.Hours, interval.Minutes, interval.Seconds);
+
+
+            return timeString;
+              }
+    }
+
+   
+   
+}
